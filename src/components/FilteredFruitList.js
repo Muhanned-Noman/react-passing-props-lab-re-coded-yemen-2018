73884f6e056bcf457be36ba878ee2e06c1098b08
Muhanned Abdulhaq Mohammed Noman <muhanned.noman@gmail.com>
2018-10-25 06:06:50 +0000

    automatically backed up by learn

--- src/components/FilteredFruitList.js
+++ src/components/FilteredFruitList.js
@@ -1,3 +1,4 @@
+
 import React, { Component } from 'react';
 
 class FilteredFruitList extends Component {
@@ -14,10 +15,8 @@ class FilteredFruitList extends Component {
       .then(response => response.json())
       .then(items => this.setState({ items }));
   }
-
   render() {
     const list = !this.props.filter || this.props.filter === 'all' ? this.state.items : this.state.items.filter(i => i.fruit_type === this.props.filter);
-
     return (
       <ul className="fruit-list">
         {list.map((item, index) => <li key={index}>{item.char}</li>)}

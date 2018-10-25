1bb4bf0cbcf767105135eafcc6abac214ceb1502
Muhanned Abdulhaq Mohammed Noman <muhanned.noman@gmail.com>
2018-10-25 06:08:22 +0000

    automatically backed up by learn

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -1,14 +1,14 @@
 import React from 'react';
 
-const Filter = ({ filters, handleChange }) => 
+const Filter = ({ allFilter, handleChange }) => 
   <select onChange={handleChange} defaultValue='all'>
     <option value='all'>All</option>
-    {filters.map(filter =>
+    {allFilter.map(filter =>
       <option key={filter} value={filter}>{filter}</option>
     )}
   </select>;
 Filter.defaultProps = {
-  filters: [],
+  allFilter: [],
   handleChange: function() {}
 };
 
